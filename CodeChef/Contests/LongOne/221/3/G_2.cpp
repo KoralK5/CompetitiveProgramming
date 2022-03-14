233f34471a1062f0bc8aa24ab5f0315233bb5be0
unknown <KoralK5>
2022-03-14 01:02:56 -0400

    23th so far

---
+++ CodeChef/Contests/LongOne/221/3/G_2.cpp
@@ -0,0 +1,160 @@
+/*
+ID: Koral Kulacoglu
+TASK: test
+LANG: C++                 
+*/
+
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+ 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef complex<ld> cd;
+ 
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pll;
+typedef pair<ld,ld> pld;
+ 
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<char> vc;
+typedef vector<ld> vld;
+typedef vector<ll> vll;
+typedef vector<pi> vpi;
+typedef vector<pll> vpll;
+typedef vector<cd> vcd;
+
+template<class T> using pq = priority_queue<T>;
+template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ 
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a,x) for (auto& a : x)
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+ 
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define fir first
+#define sec second
+#define ins insert
+#define lbound(a, v) lower_bound(all(a), v)-a.begin()
+#define ubound(a, v) upper_bound(all(a), v)-a.begin()
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a*b)/gcd(a, b)
+ 
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+ 
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+ 
+const int MOD = 1000000007;
+const char nl = '\n';
+const int MX = 1e5+2; 
+
+vector<vi> arr;
+vi vals;
+bool works;
+unordered_set<int> ss;
+int mp[MX];
+int n;
+
+void run(int start, int par, int m) {
+	if (works) return;
+
+	mp[vals[start]]++;
+	if (vals[start] < m) ss.insert(vals[start]);
+
+	trav (it, arr[start]) {
+		if (it == par) continue;
+		run(it, start, m);
+	}
+	if (sz(ss) == m) works = true;
+
+	mp[vals[start]]--;
+	if (mp[vals[start]] == 0 && ss.find(vals[start]) != ss.end()) ss.erase(vals[start]);
+}
+
+bool check(int m) {
+	ss.clear();
+	FOR (i, 0, n+1) mp[i] = 0;
+
+	works = false;
+	run(1, 0, m);
+	return works;
+}
+ 
+void solve() {
+	cin >> n;
+
+	vals.clear(); vals.resize(n+1);
+	FOR (i, 1, n+1) cin >> vals[i];
+
+	arr.clear(); arr.resize(n+1);
+	FOR (i, 0, n-1) {
+		int a, b; cin >> a >> b;
+		arr[a].pb(b);
+		arr[b].pb(a);
+	}
+	int l=1, r=n+2;
+	int ans=0;
+	while (l <= r) {
+		int m = (l+r)/2;
+		if (check(m)) {
+			ans = max(ans, m);
+			l = m+1;
+		}
+		else {
+			r = m-1;
+		}
+	}
+	cout << ans << nl;
+}
+ 
+int main() {
+    cin.tie(0)->sync_with_stdio(0); 
+    cin.exceptions(cin.failbit);
+ 
+    int T = 1;
+	cin >> T;
+
+    while(T--) {
+        solve();
+    }
+ 
+	return 0;
+}
+
